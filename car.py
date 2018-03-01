4c93a5b023e7c87f21bd0e4e2c5f61554fecc25a
saqibjr <saqibjr@hotmail.com>
2018-03-01 19:46:47 +0000

    Add files via upload

--- car.py
+++ car.py
@@ -1,34 +1,62 @@
 class Car:
 
     carActive = False
+    journeyStarted = False
     
     currentLocationX = 0
     currentLocationY = 0
 
+    startX = 0
+    startY = 0
+
     destinationX = 0
     destinationY = 0 
 
-    def startCar (self, x, y):
-        self.destinationX = x
-        self.destinationY = y
+    def startCar (self, startX, startY, destinationX, destinationY):
+
+        self.startX = startX
+        self.startY = startY
+        self.destinationX = destinationX
+        self.destinationY = destinationY
         self.carActive = True
 
     def moveCar (self):
 
-        if (currentLocationX != destinationX):
-            if (destinationX > currentLocationX):
+        if (journeyStarted == False):
+            finalX = startX
+            finalY = startY
+        else:
+            finalX = destinationX
+            finalY = destinationY 
+
+        if ((currentLocationX != finalX) || (currentLocationY != finalY)):
+            if (finalX > currentLocationX):
                 self.currentLocationX += 1
-                return
-            elif (destinationX < currentLocationX):
+            elif (finalX < currentLocationX):
                 self.currentLocationX -= 1
-                return
-            elif (destinationY > currentLocationY):
+            elif (finalY > currentLocationY):
                 self.currentLocationY += 1
-                return
-            elif (destinationY < currentLocationY):
+            elif (finalY < currentLocationY):
                 self.currentLocationY -= 1
-                return
-            if (destinationX == currentLocationX):
-                if (destinationY == currentLocationY):
+
+            if ((currentLocationX == finalX) && (currentLocationY == finalY)):
+                if ((finalX == destinationX)&&(finalY == destinationY)):
                     self.carActive = False
+                    self.journeyStarted = False
+                else:
+                    self.journeyStarted = True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
