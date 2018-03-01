5a8e5705b8b230558e3de82676574b762f513dfb
Robert Nash <email@robertnash.org>
2018-03-01 18:34:05 +0000

    Basic ride class

---
+++ ride.py
@@ -0,0 +1,20 @@
+class Ride:
+    def __init__(self, parameters):
+        parametersList = parameters.split(' ')
+        self.startX = parametersList[0]
+        self.startY = parametersList[1]
+
+        self.finishX = parametersList[2]
+        self.finishY = parametersList[3]
+
+        self.earliestStart = parametersList[4]
+        self.earliestFinish = parametersList[5]
+
+    def assignCar(car):
+        self.assignedCar = car
+
+    def assigned():
+        if self.assignedCar:
+            return self.assignedCar
+        else:
+            return False
