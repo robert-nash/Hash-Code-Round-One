b221e10cc4cd33c83ff6e65cd2b0b8994e691b14
Robert Nash <email@robertnash.org>
2018-03-01 18:40:47 +0000

    Update ride class defintions

--- ride.py
+++ ride.py
@@ -10,10 +10,10 @@ class Ride:
         self.earliestStart = parametersList[4]
         self.earliestFinish = parametersList[5]
 
-    def assignCar(car):
+    def assignCar(self,car):
         self.assignedCar = car
 
-    def assigned():
+    def assigned(self):
         if self.assignedCar:
             return self.assignedCar
         else:
