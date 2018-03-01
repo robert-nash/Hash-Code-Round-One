834fb8ed6c4b3c743aeaef0143365b368ad73246
saqibjr <saqibjr@hotmail.com>
2018-03-01 20:32:45 +0000

    Add files via upload

--- router.py
+++ router.py
@@ -1,3 +1,24 @@
+import math
+
 class Router:
-    def assignRide(self, car):
-        pass
+    def assignRide(current_ride, carList):
+        personX = current_ride.startX
+        personY = current_ride.startY
+        endX = current_ride.finishX
+        endY = current_ride.finishY
+        carDistance = -1
+        carID = -1
+        for current_car in carList:
+            if (!(current_car.carActive)):
+                carX = current_car.currentLocationX
+                carY = current_car.currentLocationY
+                pDistance = math.sqrt ((personX-carX)**2 + (personY-carY)**2)
+                if (carDistance == -1):
+                    carDistance = pDistance
+                    carID = current_car.id
+                else:
+                    if (pDistance<carDistance):
+                        carDistance = pDistance
+                        carID = current_car.id
+
+        carList[carID].startCar(personX, personY, endX, endY)
