9525d70c58bc69b9f9c7d306607f32cf3497f050
Robert Nash <email@robertnash.org>
2018-03-01 19:14:56 +0000

    ride.py PEP8 (formating)

--- ride.py
+++ ride.py
@@ -10,7 +10,7 @@ class Ride:
         self.earliestStart = parametersList[4]
         self.earliestFinish = parametersList[5]
 
-    def assignCar(self,car):
+    def assignCar(self, car):
         self.assignedCar = car
 
     def assigned(self):
