597355483e7f4afec2432a1d4f5858169692a266
saqibjr <saqibjr@hotmail.com>
2018-03-01 20:13:31 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -10,12 +10,20 @@ parameters = readFile.getParameters(inputFile)
 
 Router = router.Router()
 
+carList = []
+
+for i in range (parameters["vehicles"]):
+    carList.append (car.Car(i))
+
+for car in carList:
+    if (car.carActive):
+        car.moveCar()
+
 for current_ride in rides:
     if (not current_ride.assigned()):
-        Router.assignCar(ride)
+        Router.assignCar(current_ride, carList)
 
 # Create rides in for loop
 # Store in List
 
 # Loop through each ride, Loop through available cars
-
