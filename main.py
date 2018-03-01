876f0a6fdbe9d78b1972a6a4338388e5a7969314
saqibjr <saqibjr@hotmail.com>
2018-03-01 20:40:29 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -18,13 +18,18 @@ for i in range (parameters["vehicles"]):
 
 for j in range (parameters["steps"]):
 
-    for car in carList:
-        if (car.carActive):
-            car.moveCar()
+    for current_car in carList:
+        if (current_car.carActive):
+            current_car.moveCar()
 
     for current_ride in rides:
         if (not current_ride.assigned()):
             Router.assignCar(current_ride, carList)
+
+for vehicle in carList:
+    print (current_car.currentLocationX)
+    print (current_car.currentLocationY)
+    print ("")
 
 # Create rides in for loop
 # Store in List
